0cfbdfa31face294e0652b1d7e017a7f0c67b520
mphill422 <422mphill@gmail.com>
2026-03-31 20:25:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import statsapi
 import pandas as pd
-from datetime import datetime
-import pytz
+from datetime import datetime, timezone, timedelta
 
 st.set_page_config(page_title="Kalshi MLB Model", layout="wide")
 st.title("Kalshi MLB Run Total Model")
@@ -24,12 +23,11 @@ try:
             away_pitcher = game.get('away_probable_pitcher', 'TBD')
             game_time = game['game_datetime']
             utc = datetime.strptime(game_time, '%Y-%m-%dT%H:%M:%SZ')
-            utc = utc.replace(tzinfo=pytz.utc)
-            et = utc.astimezone(pytz.timezone('US/Eastern'))
+            et = utc - timedelta(hours=4)
             time_str = et.strftime('%I:%M %p ET')
             st.markdown("**" + away + " @ " + home + "** - " + time_str)
             st.caption("Away SP: " + away_pitcher + " | Home SP: " + home_pitcher)
             st.divider()
 
 except Exception as e:
-    st.error("Error loading schedule: " + str(e))
+    st.error("Error: " + str(e))
